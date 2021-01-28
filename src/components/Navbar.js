7a98c170cb9edcba43cff2931ecd31e65f2a6cbb
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-28 22:05:32 +0000

    Add links to navbar in desktop mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,26 @@ import styled from "styled-components";
 import HamburguerIcon from "./HamburguerIcon";
 
 import Img from "../assets/images/logo.svg";
+import Button from "./Button";
 
 function Navbar({ showMenu, setShowMenu }) {
   return (
     <Container>
       <Logo src={Img} alt="Shortly logo" />
       <HamburguerIcon showMenu={showMenu} setShowMenu={setShowMenu} />
+
+      <NavLinks>
+        <div>
+          <NavItem>Features</NavItem>
+          <NavItem>Pricing</NavItem>
+          <NavItem>Resources</NavItem>
+        </div>
+
+        <div>
+          <NavItem>Login</NavItem>
+          <LoginButton>Sign Up</LoginButton>
+        </div>
+      </NavLinks>
     </Container>
   );
 }
@@ -20,8 +34,49 @@ const Container = styled.header`
 
   width: 100%;
   padding: 2.5rem 1.5rem;
+
+  @media ${(props) => props.theme.devices.tablet} {
+    align-items: center;
+  }
 `;
 
 const Logo = styled.img``;
+
+const NavLinks = styled.div`
+  display: none;
+
+  @media ${(props) => props.theme.devices.tablet} {
+    flex: 1;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 2rem;
+
+    div {
+      display: flex;
+      align-items: center;
+
+      & > span:not(:last-child) {
+        margin-right: 2rem;
+      }
+    }
+  }
+`;
+
+const NavItem = styled.span`
+  color: ${(props) => props.theme.colors.grey_violet};
+  font-weight: 700;
+  transition: color 0.4s;
+
+  &:hover {
+    color: ${(props) => props.theme.colors.violet};
+    cursor: pointer;
+  }
+`;
+
+const LoginButton = styled(Button)`
+  padding: 0.5rem 1.5rem;
+`;
 
 export default Navbar;
