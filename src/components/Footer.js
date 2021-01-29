592defcdb99d83ef76092e8d771358cacd017bd0
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-29 18:38:04 +0000

    Complete footer for desktop view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,61 +11,74 @@ import Icon4 from "../assets/icons/iconInstagram";
 function Footer() {
   return (
     <Container>
-      <Logo src={LogoImg} alt="Shortly logo" />
+      <InnerContainer>
+        <div className="logo-wrapper">
+          <Logo src={LogoImg} alt="Shortly logo" />
+        </div>
 
-      <div>
-        <Links>
-          <h1>Features</h1>
+        <div className="links-wrapper">
+          <Links>
+            <h1>Features</h1>
 
-          <span>Link Shortening</span>
-          <span>Branded Links</span>
-          <span>Analytics</span>
-        </Links>
+            <span>Link Shortening</span>
+            <span>Branded Links</span>
+            <span>Analytics</span>
+          </Links>
 
-        <Links>
-          <h1>Resources</h1>
+          <Links>
+            <h1>Resources</h1>
 
-          <span>Blog</span>
-          <span>Developers</span>
-          <span>Support</span>
-        </Links>
+            <span>Blog</span>
+            <span>Developers</span>
+            <span>Support</span>
+          </Links>
 
-        <Links>
-          <h1>Company</h1>
+          <Links>
+            <h1>Company</h1>
 
-          <span>About</span>
-          <span>Our Team</span>
-          <span>Careers</span>
-          <span>Contact</span>
-        </Links>
+            <span>About</span>
+            <span>Our Team</span>
+            <span>Careers</span>
+            <span>Contact</span>
+          </Links>
 
-        <SocialLinks>
-          <IconWrapper>
-            <Icon1 />
-          </IconWrapper>
+          <SocialLinks>
+            <IconWrapper href="https://facebook.com">
+              <Icon1 />
+            </IconWrapper>
 
-          <IconWrapper>
-            <Icon2 />
-          </IconWrapper>
+            <IconWrapper href="https://twitter.com">
+              <Icon2 />
+            </IconWrapper>
 
-          <IconWrapper>
-            <Icon3 />
-          </IconWrapper>
+            <IconWrapper href="https://pinterest.com">
+              <Icon3 />
+            </IconWrapper>
 
-          <IconWrapper>
-            <Icon4 />
-          </IconWrapper>
-        </SocialLinks>
-      </div>
+            <IconWrapper href="https://instagram.com">
+              <Icon4 />
+            </IconWrapper>
+          </SocialLinks>
+        </div>
+      </InnerContainer>
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.dark_violet};
+`;
+
+const InnerContainer = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
+  width: min(100%, 1240px);
   background-color: ${(props) => props.theme.colors.dark_violet};
 
   padding: 4rem 2rem;
@@ -76,6 +89,26 @@ const Container = styled.div`
 
     & > div:not(:last-child) {
       margin-bottom: 2.5rem;
+    }
+  }
+
+  @media ${(props) => props.theme.devices.tablet} {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: space-between;
+
+    .links-wrapper {
+      flex-direction: row;
+      flex: 2;
+      justify-content: space-between;
+
+      & > div:not(:last-child) {
+        margin: 0 3rem 0 0;
+      }
+    }
+
+    .logo-wrapper {
+      flex: 1;
     }
   }
 `;
@@ -98,21 +131,32 @@ const Links = styled.div`
   }
 
   span {
-    color: ${(props) => props.theme.colors.grey};
+    color: ${(props) => props.theme.colors.grey_violet};
     margin-bottom: 0.3rem;
+    transition: color 0.2s;
+
+    &:hover {
+      cursor: pointer;
+      color: ${(props) => props.theme.colors.cyan};
+    }
+  }
+
+  @media ${(props) => props.theme.devices.tablet} {
+    align-items: flex-start;
   }
 `;
 
 const SocialLinks = styled.div`
   display: flex;
   align-items: center;
+  align-self: flex-start;
 
   > *:not(:last-child) {
     margin-right: 1.5rem;
   }
 `;
 
-const IconWrapper = styled.div`
+const IconWrapper = styled.a`
   color: ${(props) => props.theme.colors.white};
   transition: color 0.2s;
 
